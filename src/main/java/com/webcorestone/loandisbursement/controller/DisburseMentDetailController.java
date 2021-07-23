c95e5a627416245d6e39cd1d0e0e1bacde92f248
akashdkalanke <akashkalanke32@gmail.com>
2021-07-23 22:11:15 +0530

    disbursemodule

--- src/main/java/com/webcorestone/loandisbursement/controller/DisburseMentDetailController.java
+++ src/main/java/com/webcorestone/loandisbursement/controller/DisburseMentDetailController.java
@@ -78,7 +78,7 @@ public class DisburseMentDetailController
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
 				
-				return new PasswordAuthentication("akashkalanke71@gmail.com","akash@1992");
+				return new PasswordAuthentication("akashkalanke71@gmail.com","**********");
 			}
 		});
 		
